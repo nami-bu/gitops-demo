e248eb21e4de4b3eb23ce746bde9b8c03e1434b3
landians <1309603587@qq.com>
2025-04-20 14:18:59 +0800

    save

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello from Kubernetes with Argo CD 🚀")
 	})
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "I'm health now, don't worry!")
+	})
 	fmt.Println("Listening on :8080")
 	http.ListenAndServe(":8080", nil)
 }
