fbea3161b71ad20b7976c5ebbf724d65264a2b1c
landians <1309603587@qq.com>
2025-04-20 15:18:36 +0800

    port 8080

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	data := map[string]string{
+		"S": "Congratulations!",
+		"U": "Good Job!",
+		"N": "Take a rest!",
+	}
+
+	v, _ := json.Marshal(data)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello from Kubernetes with Argo CD 🚀")
 	})
@@ -14,6 +23,9 @@ func main() {
 	})
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "CPU: 80%, GPU: 10%, Memory: 20%")
+	})
+	http.HandleFunc("/say", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(v)
 	})
 	fmt.Println("Listening on :8080")
 	http.ListenAndServe(":8080", nil)
