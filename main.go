b0248d96f48f1e639715ad76471eac1890ba948b
landians <1309603587@qq.com>
2025-04-20 15:01:01 +0800

    fix: deploy.yml

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 		fmt.Fprintln(w, "I'm health now, don't worry!")
 	})
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "CPU: 80%, GPU: 15%, Memory: 20%")
+		fmt.Fprintln(w, "CPU: 80%, GPU: 10%, Memory: 20%")
 	})
 	fmt.Println("Listening on :8080")
 	http.ListenAndServe(":8080", nil)
